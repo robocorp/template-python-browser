069f8fad88d1669e796217bc6ac1f8bc0d82e93e
Cosmin Poieana <cosmin@robocorp.com>
2024-01-30 16:24:05 +0300

    Cosmetics and idiomatic Python refactoring

--- tasks.py
+++ tasks.py
@@ -1,22 +1,23 @@
+import os
+from pathlib import Path
+
+import requests
 from robocorp import browser
 from robocorp.tasks import task
 from RPA.Excel.Files import Files as Excel
 
-from pathlib import Path
-import os
-import requests
 
 FILE_NAME = "challenge.xlsx"
-OUTPUT_DIR = Path(os.environ.get('ROBOT_ARTIFACTS'))
 EXCEL_URL = f"https://rpachallenge.com/assets/downloadFiles/{FILE_NAME}"
+OUTPUT_DIR = Path(os.getenv("ROBOT_ARTIFACTS", "output"))
 
 
 @task
 def solve_challenge():
-    """
-    Solve the RPA challenge
-    
-    Downloads the source data excel and uses Playwright to solve rpachallenge.com from challenge
+    """Main task which solves the RPA challenge!
+
+    Downloads the source data Excel file and uses Playwright to fill the entries inside
+    rpachallenge.com.
     """
     browser.configure(
         browser_engine="chromium",
@@ -24,47 +25,48 @@ def solve_challenge():
         headless=True,
     )
     try:
+        # Reads a table from an Excel file hosted online.
         excel_file = download_file(EXCEL_URL, OUTPUT_DIR, FILE_NAME)
         excel = Excel()
         excel.open_workbook(excel_file)
         rows = excel.read_worksheet_as_table("Sheet1", header=True)
 
+        # Surf the automation challenge website and fill in information from the table
+        #  extracted above.
         page = browser.goto("https://rpachallenge.com/")
         page.click("button:text('Start')")
         for row in rows:
-            fill_and_submit_form(row)
+            fill_and_submit_form(row, page=page)
         element = page.locator("css=div.congratulations")
         browser.screenshot(element)
-
     finally:
-        # Place for teardown and cleanups
-        # Playwright handles browser closing
-        print('Done')
+        # A place for teardown and cleanups. (Playwright handles browser closing)
+        print("Automation finished successfully!")
 
 
-def download_file(url: str, target_dir: Path, target_filename:str) -> str:
-    """ 
-    Downloads a file from the given url into the given folder with given filename. 
-    """
-    target_dir.mkdir(exist_ok=True)
+def download_file(url: str, target_dir: Path, target_filename: str) -> Path:
+    """Downloads a file from the given URL into a custom folder & name."""
+    # Obtain the content of the file hosted online.
     response = requests.get(url)
-    response.raise_for_status()  # This will raise an exception if the request fails
-    local_filename = Path(target_dir, target_filename)
-    with open(local_filename, 'wb') as f:
-        f.write(response.content)  # Write the content of the response to a file
-    return local_filename
+    response.raise_for_status()  # this will raise an exception if the request fails
+    # Write the content of the request response to the target file.
+    target_dir.mkdir(exist_ok=True)
+    download_file = target_dir / target_filename
+    download_file.write_bytes(response.content)
+    return download_file
 
 
-def fill_and_submit_form(row):
-    """
-    Fills a single form with the information of a single row in the Excel
-    """
-    page = browser.page()
-    page.fill("//input[@ng-reflect-name='labelFirstName']", str(row["First Name"]))
-    page.fill("//input[@ng-reflect-name='labelLastName']", str(row["Last Name"]))
-    page.fill("//input[@ng-reflect-name='labelCompanyName']", str(row["Company Name"]))
-    page.fill("//input[@ng-reflect-name='labelRole']", str(row["Role in Company"]))
-    page.fill("//input[@ng-reflect-name='labelAddress']", str(row["Address"]))
-    page.fill("//input[@ng-reflect-name='labelEmail']", str(row["Email"]))
-    page.fill("//input[@ng-reflect-name='labelPhone']", str(row["Phone Number"]))
+def fill_and_submit_form(row: dict, *, page: browser.Page):
+    """Fills a single form with the information of a single row from the table."""
+    field_data_map = {
+        "labelFirstName": "First Name",
+        "labelLastName": "Last Name",
+        "labelCompanyName": "Company Name",
+        "labelRole": "Role in Company",
+        "labelAddress": "Address",
+        "labelEmail": "Email",
+        "labelPhone": "Phone Number"
+    }
+    for field, key in field_data_map.values():
+        page.fill(f"//input[@ng-reflect-name='{field}']", row[key])
     page.click("input:text('Submit')")
